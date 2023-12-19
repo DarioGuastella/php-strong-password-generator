f38e6ac38322f36477c4f3f57123bbb3b6e12ebf
DarioGuastella <jimmyguastella@gmail.com>
2023-12-19 17:57:17 +0100

    prova con 3 stringhe

--- functions.php
+++ functions.php
@@ -11,3 +11,21 @@ function randomPassword()
     }
     return implode($pass);
 }
+// function randomPassword()
+// {
+//     $password = $_GET["password"];
+//     $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     $numbers = '1234567890';
+//     $symbols = '!.,$%&/()&/';
+//     $pass = array();
+//     $alphabetLength = strlen($alphabet) - 1;
+//     $numbersLength = strlen($numbers) - 1;
+//     $symbolsLength = strlen($symbols) - 1;
+//     for ($i = 0; $i < $password / 3; $i++) {
+//         $letters = rand(1, $alphabetLength);
+//         $ranNumbers = rand(1, $numbersLength);
+//         $ranSymbols = rand(1, $symbolsLength);
+//         $pass[] = $alphabet[$letters] . $numbers[$ranNumbers] . $symbols[$ranSymbols];
+//     }
+//     return implode($pass);
+// }
